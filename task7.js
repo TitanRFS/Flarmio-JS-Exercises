64f57ce70f3a86b6b33a9e132edd0ed3129fb8bc
Christos Gianniotis <115632527+TitanRFS@users.noreply.github.com>
2025-03-04 13:47:05 +0200

    add task7 part-1 and part-2

---
+++ task7.js
@@ -0,0 +1,43 @@
+// Part 1 of Task 7
+// const basket = ["apple", "apple", "orange", "orange"];
+
+// const price = {
+//   apple: 12,
+//   orange: 32,
+//   pen: 22,
+//   notebook: 89,
+//   peach: 18,
+// };
+
+// const total = basket.reduce((acc, item) => {
+//   return acc + price[item];
+// }, 0);
+
+// console.log(total);
+
+// Part 2 of Task 7
+// const basket = [
+//     "apple",
+//     "pen",
+//     "peach",
+//     "apple",
+//     "pen",
+//     "peach",
+//     "notebook",
+//     "notebook",
+//     "orange",
+//   ];
+
+//   const price = {
+//     apple: 12,
+//     orange: 32,
+//     pen: 22,
+//     notebook: 89,
+//     peach: 18,
+//   };
+
+//   const total = basket.reduce((acc, item) => {
+//     return acc + price[item];
+//   }, 0);
+
+//   console.log(total);
